0139d7f2ad647e81812219c569b7e52b63f38415
jawahendy <hendyn25@gmail.com>
2019-08-26 16:28:54 +0700

    test2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const keys = require('./config/keys');
 
 const app = express();
 
@@ -9,7 +9,16 @@ const app = express();
 
 //clientID :91233973303-1rsiae9u34u27cghp4k50ena6v2hu3m3.apps.googleusercontent.com
 //clientSecret : nSXhuuLjmAuqP_NjaT7WTM9T
-passport.use(new GoogleStrategy());
+
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret : keys.googleClientSecret,
+    callbackURL : '/auth/goolge/callback'
+}, (accesToken) =>{
+    console.log(accesToken);
+})
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

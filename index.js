d872f4577aec9f24bf768dcfe4c29649016d9a4d
jawahendy <hendyn25@gmail.com>
2019-08-26 15:44:39 +0700

    test

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+// console.developers.google.com
+
+//clientID :91233973303-1rsiae9u34u27cghp4k50ena6v2hu3m3.apps.googleusercontent.com
+//clientSecret : nSXhuuLjmAuqP_NjaT7WTM9T
 passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
